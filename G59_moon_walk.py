9fff58fc7c0f15057f8655ed8c0e42c85cf6fe9c
J0RdN-w <yordanoswuletaw@gmail.com>
2024-03-21 19:24:11 +0300

    initial commit

---
+++ G59_moon_walk.py
@@ -0,0 +1,63 @@
+people = [
+    "Biniyam Negasa Supa",
+    "Yonas Sintayehu Abebe",
+    "Yeneineh Seiba Tonja",
+    "Wondmeneh Dereje Dadi",
+    "Chera Mihiretu Tadese",
+    "Kaleb Wondimu Wotale",
+    "Kuma Telila Bacha",
+    "Samuel Geremew Keno",
+    "Kidist Bezabih Wakgari",
+    "Yohannes Welel Atnafu",
+    "Eyob Tariku Faro",
+    "Samuel Fikadesilassie Legesse",
+    "Fasil Hawultie Admasie",
+    "Tamirat Kebede Worati",
+    "Duressa Shukuri Anota",
+    "Eyasu Yidnekachew Asres",
+    "Asegid Adane Daba",
+]
+
+def round_robin_pairing(n):
+    if n % 2 != 0:
+        n += 1  # If the number of students is odd, add an extra student with a placeholder
+
+    students = list(range(1, n + 1))
+
+    # Create a list to store pairs for each day
+    pairs_per_day = []
+
+    # Generate pairs for each day
+    for day in range(n - 1):
+        pairs = []
+        for i in range(n // 2):
+            pair = (students[i], students[n - 1 - i])
+            pairs.append(pair)
+        pairs_per_day.append(pairs)
+
+        # Rotate the students for the next day
+        students = [students[0]] + [students[-1]] + students[1:-1]
+
+    # If an extra student was added, remove the placeholder
+    if n % 2 != 0:
+        for pairs in pairs_per_day:
+            pairs[-1] = (pairs[-1][0], None)
+
+    return pairs_per_day
+
+
+# Example usage with 4 students
+n_students = len(people)
+pairs_schedule = round_robin_pairing(n_students)
+
+# Display the pairs for each day
+# for day, pairs in enumerate(pairs_schedule, start=1):
+#     for 
+print("Hey team,  it's time for moonwalk 🌝 🚶‍♀️. Find your partner below and get to know each other 😊\n")
+day = 3
+n = len(people)
+count = 0
+for i, j in pairs_schedule[day]:
+    count+=1
+    print(f'{people[i-1]} <> {people[(j-1)%n]}')
+print(count)
